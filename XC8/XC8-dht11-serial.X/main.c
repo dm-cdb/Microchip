4f5a3cfbc8e680c658caf10bb78278da894ce111
Christian <cdebalorre@telemack.net>
2026-01-06 19:17:41 +0100

    refactor main code

--- XC8/XC8-dht11-serial.X/main.c
+++ XC8/XC8-dht11-serial.X/main.c
@@ -4,11 +4,10 @@
  *
  * !!! IMPORTANT : this code will only work with FOSC >= 8MHz !!!
  * DHT11 send 40 bits = 5 bytes
- * byte[0] (integer) = RH ; BYTE[1] (decimal) = 0 : byte[2] (integer) = T°C
- * byte[3] (decimal)
+ * byte[0] (integer) = RH integer ; byte[1] = RH decimal ; byte[2] = T°C integer
+ * byte[3] = T°C decimal ; byte[4] = checksum
  * Most of DHT11 clones, not from ASAIR/AOSONG will set this byte to 0.
  * On guenine ASAIR parts : the first bit indicates the negative T° when set to 1.
- * This code presently does not check for negative T°. 
  * Created on 21 octobre 2024, 05:02
  */
 
@@ -43,19 +42,24 @@
 #define DHTPIN GPIObits.GP0
 #define DHTDIRPIN TRISIObits.TRISIO0
 
-unsigned const char rn[] = "\r\n"; // Char line feed
-unsigned const char errMsg1[] = "Data LOW error";
-unsigned const char errMsg2[] = "Data HIGH error";
-unsigned const char errMsg3[] = "Sensor not responding";
-unsigned const char errMsg4[] = "Checksum error";
-static volatile unsigned char txBuffer __at(0x70); //Forced in shared ram - for debug
+const unsigned char rn[] = "\r\n"; // Char line feed
+const unsigned char errMsg1[] = "Data LOW error";
+const unsigned char errMsg2[] = "Data HIGH error";
+const unsigned char errMsg3[] = "Sensor not responding";
+const unsigned char errMsg4[] = "Checksum error";
+const unsigned char errMsg5[] = "No Ack from sensor";
+static volatile unsigned char txBuffer;
 static volatile __bit TXFLAG;
 
-unsigned char temperature[] = "Temp = 00.0 C  ";
-unsigned char humidity[] = "RH   = 00.0 %  ";
-unsigned char status;
+const unsigned char perc = 0x25, 
+        cels = 0x43, 
+        space = 0x20, 
+        sep = 0x2c;
+
+const unsigned char temperature[] = "Temp = ";
+const unsigned char humidity[] = "RH   = ";
 // data = RH integer byte, RH decimal byte (always 0), T° integer byte, T° decimal byte, checksum
-unsigned char data[5] __at(0xa0); //Forced in shared ram - for debug
+unsigned char data[5] __at(0x70); //Forced in shared ram - for debug
 
 //Declare functions
 void picInit(void);
@@ -63,7 +67,7 @@ void tx_UART(void);
 void send_Txt(const unsigned char *, unsigned char);
 void helo(void); // Start com with sensor
 __bit ehlo(void); // Ack start with sensor
-char readData(unsigned char*);
+bool readDHT11(void);
 
 void tx_UART() {
     TXFLAG = 1;
@@ -112,42 +116,51 @@ void helo() {
 __bit ehlo() {
     TMR2 = 0;
     while (DHTPIN && TMR2 < 100) { // Check if DHTPIN still HIGH after helo
-        if (TMR2 > 99) return 0; // Ehlo timeout - sensor not ready
+        if (TMR2 > 99) {
+            send_Txt(errMsg5, sizeof (errMsg5));
+            send_Txt(rn, sizeof (rn));
+            return 0;
+        } // Ehlo timeout - sensor not ready
     }
 
     TMR2 = 0;
     while (!DHTPIN && TMR2 < 200) { // Ehlo 80µs LOW response 
-        if (TMR2 > 199) return 0;
+        if (TMR2 > 199) {
+            send_Txt(errMsg5, sizeof (errMsg5));
+            send_Txt(rn, sizeof (rn));
+            return 0;
+        }
     }
 
     TMR2 = 0;
     while (DHTPIN && TMR2 < 200) { // Ehlo 80µs HIGH response 
-        if (TMR2 > 199) return 0;
+        if (TMR2 > 199) {
+            send_Txt(errMsg5, sizeof (errMsg5));
+            send_Txt(rn, sizeof (rn));
+            return 0;
+        }
     }
     return 1; // Ehlo OK ; data transmission follow
 }
 
-char readData(unsigned char data[]) {
-    unsigned char Idx = 0;
-    while (Idx < 5) {
-        unsigned char value = 0;
-        for (char i = 0; i < 8; i++) {
-            TMR2 = 0;
-            while (!DHTPIN) {
-                if (TMR2 > 150) return 1; // LOW between bits = 50µs 
+bool readDHT11() {
+    for (uint8_t Idx = 0; Idx < 5; Idx++) {
+        for (uint8_t a = 0; a < 8; a++) {
+            while (!DHTPIN); // Should last 50µs
+            __delay_us(50);
+            if (DHTPIN) { // If DHTPIN still HIGH bit = 1
+                data[Idx] |= (1 << (7 - a));
             }
-            TMR2 = 0;
-            while (DHTPIN) { // Assess HIGH duration
-                if (TMR2 > 254) return 2;
-            }
-            if (TMR2 > 80) // bit 0 = 26-28µs - bit 1 = 70µs
-                value |= (1 << (7 - i)); // Populate value byte - MSB first
+            while (DHTPIN);
         }
-        data[Idx] = value;
-        Idx++;
     }
-
-    return 0; // Read byte OK
+    uint8_t checksum = ((data[0] + data[1] + data[2] + data[3])) & 0xff;
+    if (!(checksum == data[4])) {
+        send_Txt(errMsg4, sizeof (errMsg4));
+        send_Txt(rn, sizeof (rn));
+        return 0;
+    }
+    return 1; // bytes OK
 }
 
 void picInit() {
@@ -170,38 +183,67 @@ void main(void) {
     T2CONbits.TMR2ON = 1;
 
     while (true) {
+        for (uint8_t i = 0; i < 5; i++) data[i] = 0;
+        unsigned char tens = 0, units = 0, dec = 0, sign = 0;
+        
         helo();
         if (ehlo()) { // Test if sensor is alive
-            status = readData(data);
-            switch (status) {
-                case 1:
-                    send_Txt(errMsg1, sizeof (errMsg1));
-                    send_Txt(rn, sizeof (rn));
-                    break;
-                case 2:
-                    send_Txt(errMsg2, sizeof (errMsg2));
-                    send_Txt(rn, sizeof (rn));
-                    break;
-            }
-            if (data[4] == (data[0] + data[1] + data[2] + data[3])) {
-                temperature[7] = data[2] / 10 + '0';
-                temperature[8] = data[2] % 10 + '0';
-                temperature[10] = data[3] + '0';
-                humidity[7] = data[0] / 10 + '0';
-                humidity[8] = data[0] % 10 + '0';
-                humidity[10] = data[1] / 10 + '0';
-                send_Txt(temperature, sizeof (temperature));
-                send_Txt(rn, sizeof (rn));
-                send_Txt(humidity, sizeof (humidity));
-                send_Txt(rn, sizeof (rn));
+            if (readDHT11()) {
+                if ((data[3] >> 7)) {
+                    sign = '-';
+                    data[3] = data[3] & 0x7f; // Remove negative bit
+                } else sign = '+';
             } else {
-                send_Txt(errMsg4, sizeof (errMsg4));
+                send_Txt(errMsg3, sizeof (errMsg3));
                 send_Txt(rn, sizeof (rn));
             }
-        } else {
-            send_Txt(errMsg3, sizeof (errMsg3));
-            send_Txt(rn, sizeof (rn));
         }
+        // Format RH
+        if (data[0] >= 10) {   // Tens from raw data
+            for (char i = 0; data[0] >= 10; i++) {
+                data[0] -= 10;
+                tens = i + 1;
+            }
+        }        
+         units = data[0];      
+        // ASCII conversion
+        tens = tens + '0';
+        units = units + '0';
+
+        
+        // Display result on serial 
+        send_Txt(humidity, sizeof (humidity));
+        send_Txt(&space, sizeof (space));
+        send_Txt(&tens, sizeof (tens));
+        send_Txt(&units, sizeof (units));
+        send_Txt(&space, sizeof (space));
+        send_Txt(&perc, sizeof (perc));
+        send_Txt(rn, sizeof (rn));
+        
+        // Format Temp
+        tens = 0, units = 0, dec = 0;
+        if (data[2] >= 10) {   // Tens from raw data
+            for (char i = 0; data[2] >= 10; i++) {
+                data[2] -= 10;
+                tens = i + 1;
+            }
+        }        
+         units = data[2];      
+        // ASCII conversion
+        tens = tens + '0';
+        units = units + '0';
+        data[3] = data[3] + '0';
+        
+        send_Txt(temperature, sizeof (temperature));
+        send_Txt(&sign, sizeof(sign));
+        send_Txt(&tens, sizeof tens);
+        send_Txt(&units, sizeof units);
+        send_Txt(&sep, sizeof (sep));
+        send_Txt(&data[3], sizeof (data[3]));
+        send_Txt(&space, sizeof (space));
+        send_Txt(&cels, sizeof (cels));
+        send_Txt(rn, sizeof (rn));           
+                
         //built-in reminder
         __nop();
         __delay_ms(3000);
