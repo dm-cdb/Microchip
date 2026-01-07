bad1918c5dc71d6b4b8d2b46fb6c6f08c11bc78f
Christian <cdebalorre@telemack.net>
2026-01-07 19:53:22 +0100

    add dht22 code

---
+++ XC8/XC8-dht22-serial.X/main.c
@@ -0,0 +1,275 @@
+/*
+ * File:   main.c
+ * Author: Christian de Balorre
+ *
+ * !!! IMPORTANT : this code will only work with FOSC >= 8MHz !!!
+ * DHT22 send 40 bits = 5 bytes
+ * byte[0] (integer) = RH integer ; byte[1] = RH decimal ; byte[2] = T캜 integer
+ * byte[3] = T캜 decimal ; byte[4] = checksum
+ * The first bit of byte[2] indicates the negative T� when set to 1.
+ * This code presently does not check for negative T�. 
+ * Created on 21 octobre 2024, 05:02
+ */
+
+// CONFIG register (FUSES)
+// INTOSCIO oscillator                 : I/O function on GPIO4 & GPIO5
+// INTOSCCLK oscillator                : FOSC/4 output on GPIO4 CLKOUT, I/O on GP5
+// Watchdog Timer Enable bit           : disabled (WDTE)
+// Power-up Timer Enable bit           : enabled (PWRT)
+// MCLR Reset Pin Function Select bit  : disabled (MCLRE)
+// Code Protection bit                 : disabled (CP))
+// Data Code Protection bit            : disabled (CPD)
+// Brown Out Detect                    : disabled (BOREN)
+// Internal External Switchover bit    : disabled (IESO)
+// Fail-Safe Clock Monitor Enabled bit : disabled (FCMEN)
+#pragma config FOSC = INTOSCIO, WDTE = OFF, PWRTE = ON, MCLRE = OFF 
+#pragma config CP = OFF, CPD = OFF, BOREN = OFF, IESO = OFF, FCMEN = OFF
+
+#include <xc.h>
+#include <stdbool.h>
+
+#define _XTAL_FREQ  8000000     // Needed for built-in  __delay_us(x) & __delay_ms(x)
+#define FOSC _XTAL_FREQ         // Fosc in Hz - for information, not used in this project
+#define TOSC 1/FOSC             // Period = 0,125 탎
+#define FCYC FOSC/4             // Instruction freq - one instruction consumes 4 clocks
+#define TCYC 1/FCYC             // Instruction period (0,5탎 for 8MHz FOSC)
+
+#define BAUDR 9600              // Baud rate = 9600 bps -> 1 / 9600 = 104탎/bit
+#define BITDY  208              // Bit delay : 104 * 2 @ 8MHz
+#define DATABITS 8
+#define TX GPIObits.GP1         // Serial TX pin
+
+#define DHTPIN GPIObits.GP0
+#define DHTDIRPIN TRISIObits.TRISIO0
+
+const unsigned char rn[] = "\r\n"; // Char line feed
+const unsigned char errMsg1[] = "EHLO LOW error";
+const unsigned char errMsg2[] = "EHLO HIGH error";
+const unsigned char errMsg3[] = "Sensor not responding";
+const unsigned char errMsg4[] = "Checksum error";
+const unsigned char errMsg5[] = "No Ack from sensor";
+static volatile unsigned char txBuffer;
+static volatile __bit TXFLAG;
+
+const unsigned char perc = 0x25, 
+        cels = 0x43, 
+        space = 0x20, 
+        sep = 0x2c;
+
+const unsigned char temperature[] = "Temp = ";
+const unsigned char humidity[] = "RH   = ";
+// data = RH integer byte, RH decimal byte (always 0), T� integer byte, T� decimal byte, checksum
+unsigned char data[5] __at(0x70); //Forced in shared ram - for debug
+
+
+//Declare functions
+void picInit(void);
+void tx_UART(void);
+void send_Txt(const unsigned char *, unsigned char);
+void helo(void); // Start com with sensor
+__bit ehlo(void); // Ack start with sensor
+bool readDHT22();
+
+void tx_UART() {
+    TXFLAG = 1;
+    PR2 = BITDY;
+    PIR1bits.TMR2IF = 0;
+    TX = 0; // Send Start Bit
+    TMR2 = 0; // Reset timer2
+    while (!PIR1bits.TMR2IF); // Run for BITDY delay
+    PIR1bits.TMR2IF = 0;
+    for (unsigned char i = 1; i; i = (unsigned char) (i << 1)) {
+        (txBuffer & i) ? (TX = 1) : (TX = 0);
+        while (!PIR1bits.TMR2IF);
+        PIR1bits.TMR2IF = 0;
+    }
+    TX = 1; // Send Stop bit
+    while (!PIR1bits.TMR2IF);
+    PIR1bits.TMR2IF = 0;
+    // House keeping
+    txBuffer = 0x00;
+    TMR2 = 0;
+    PR2 = 0xFF;
+    TXFLAG = 0;
+
+    return;
+}
+
+void send_Txt(const unsigned char * s, unsigned char k) {
+    for (unsigned char i = 0; i < k; i++) {
+        txBuffer = s[i];
+        tx_UART();
+    }
+    txBuffer = 0x00;
+
+    return;
+}
+
+void helo() {
+    DHTDIRPIN = 0; // Set DHT com pin OUTPUT
+    DHTPIN = 0; // Set DHT com line LOW 
+    __delay_ms(2); // Helo signal LOW >= 1ms
+    DHTDIRPIN = 1; // Set DHT com pin high Z, line HIGH
+    __delay_us(20); // DHT ehlo response should come within 20-40탎
+
+}
+
+__bit ehlo() {
+    TMR2 = 0;
+    while (DHTPIN && TMR2 < 100) { // Check if DHTPIN still HIGH after helo
+        if (TMR2 > 99) {
+            send_Txt(errMsg5, sizeof (errMsg5));
+            send_Txt(rn, sizeof (rn));
+            return 0; // Ehlo timeout - sensor not ready
+        }
+    }
+
+    TMR2 = 0;
+    while (!DHTPIN && TMR2 < 200) { // Ehlo 80탎 LOW response 
+        if (TMR2 > 199) {
+            send_Txt(errMsg1, sizeof (errMsg1));
+            send_Txt(rn, sizeof (rn));
+            return 0;
+        }
+    }
+
+    TMR2 = 0;
+    while (DHTPIN && TMR2 < 200) { // Ehlo 80탎 HIGH response 
+        if (TMR2 > 199) {
+            send_Txt(errMsg2, sizeof (errMsg2));
+            send_Txt(rn, sizeof (rn));
+            return 0;
+        }
+    }
+    TMR2 = 0;
+    return 1; // Ehlo OK ; data transmission follow
+}
+
+bool readDHT22() {
+    for (uint8_t Idx = 0; Idx < 5; Idx++) {
+        for (uint8_t a = 0; a < 8; a++) {
+            while (!DHTPIN); // Should last 50탎
+            __delay_us(50);
+            if (DHTPIN) { // If DHTPIN still HIGH bit = 1
+                data[Idx] |= (1 << (7 - a));
+            }
+            while (DHTPIN);
+        }
+    }
+    uint8_t checksum = ((data[0] + data[1] + data[2] + data[3])) & 0xff;
+    if (!(checksum == data[4])) {
+        send_Txt(errMsg4, sizeof (errMsg4));
+        send_Txt(rn, sizeof (rn));
+        return 0;
+    }
+    return 1;
+}
+
+void picInit() {
+    OSCCONbits.IRCF = 0b111; // Select 8MHz clock - TOSC 125ns, TCY 0,5탎
+    CMCON0bits.CM = 0b111; // Comparator OFF
+    VRCONbits.VREN = 0; // Disable comparator internal VREF
+    ANSEL = 0x00; // Force all GPIO pins to digital mode
+    TRISIO = 0b00111101; // GPIO 1 outputs (0)
+    T2CON = 0b00000000; // Timer 2 pre & post scaler 1:1 => 1 tick = 0,5탎 @ 8MHz
+    PR2 = 0xFF;
+    TX = 1;
+
+    return;
+}
+
+void main(void) {
+    picInit();
+    txBuffer = 0x00;
+    T2CONbits.TMR2ON = 1;
+
+    while (true) {
+        for (uint8_t i = 0; i < 5; i++) data[i] = 0;
+        uint16_t resh = 0, rest = 0;
+        unsigned char hunds = 0, tens = 0, dec = 0, sign = 0;
+        helo();
+        if (ehlo()) { // Test if sensor is alive
+            if (readDHT22()) {
+                if ((data[2] >> 7)) {
+                    sign = '-';
+                    data[2] = data[2] & 0x7f; // Remove negative bit
+                } else sign = '+';
+                resh |= (uint16_t) (data[0] << 8);
+                resh |= (uint16_t) data[1];
+                rest |= (uint16_t) (data[2] << 8);
+                rest |= (uint16_t) data[3];
+            } else {
+                send_Txt(errMsg3, sizeof (errMsg3));
+                send_Txt(rn, sizeof (rn));
+            }
+        }
+        // Format RH
+        
+        if (resh >= 100) {  // Hundreds from raw data
+            for (char i = 0; resh >= 100; i++) {
+                resh -= 100;
+                hunds = i + 1;
+            }
+        }        
+        if (resh >= 10) {   // Tens from raw data
+            for (char i = 0; resh >= 10; i++) {
+                resh -= 10;
+                tens = i + 1;
+            }
+        }        
+        dec = (unsigned char) resh; // Unit from raw data
+        
+        // ASCII conversion
+        hunds = hunds + '0';
+        tens = tens + '0';
+        dec = dec + '0';
+        
+        // Display result on serial 
+        send_Txt(humidity, sizeof (humidity));
+        send_Txt(&space, sizeof (space));
+        send_Txt(&hunds, sizeof (hunds));
+        send_Txt(&tens, sizeof (tens));
+        send_Txt(&sep, sizeof (sep));
+        send_Txt(&dec, sizeof (dec));
+        send_Txt(&space, sizeof (space));
+        send_Txt(&perc, sizeof (perc));
+        send_Txt(rn, sizeof (rn));
+        
+        // Format Temperature
+        hunds = 0, tens = 0, dec = 0;
+        if (rest >= 100) {  // Hundreds from raw data
+            for (char i = 0; rest >= 100; i++) {
+                rest -= 100;
+                hunds = i + 1;
+            }
+        }
+        if (rest >= 10) {   // Tens from raw data
+            for (char i = 0; rest >= 10; i++) {
+                rest -= 10;
+                tens = i + 1;
+            }
+        }
+        dec = (unsigned char) rest; // Unit from raw data
+        
+        // ASCII conversion
+        hunds = hunds + '0';
+        tens = tens + '0';
+        dec = dec + '0';
+
+        // Display result on serial 
+        send_Txt(temperature, sizeof (temperature));
+        send_Txt(&sign, sizeof(sign));
+        send_Txt(&hunds, sizeof (hunds));
+        send_Txt(&tens, sizeof (tens));
+        send_Txt(&sep, sizeof (sep));
+        send_Txt(&dec, sizeof (dec));
+        send_Txt(&space, sizeof (space));
+        send_Txt(&cels, sizeof (cels));
+        send_Txt(rn, sizeof (rn));
+
+        //built-in reminder
+        __nop();
+        __delay_ms(3000);
+        __delay_us(1000);
+    }
+}
