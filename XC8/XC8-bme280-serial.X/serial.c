81c77fe75c1b588499850b60015d19b8663c26bf
Christian <cdebalorre@telemack.net>
2026-07-11 14:34:51 +0200

    serial bug correction

--- XC8/XC8-bme280-serial.X/serial.c
+++ XC8/XC8-bme280-serial.X/serial.c
@@ -81,7 +81,7 @@ void tx_UART() {
 void send_Txt(const unsigned char * s, unsigned char k) {
     for (unsigned char i = 0; i < k; i++) {
         txBuffer = s[i];
-        tx_UART();
+        if(txBuffer) tx_UART();  // Discard end-of-string 0
     }
      
     return;
